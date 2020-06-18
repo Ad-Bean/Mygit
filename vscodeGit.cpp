d3e1779b0e51b22653cd9c2cd1b0c2a91433ed96
adbean <adbeanx@outlook.com>
2020-06-18 23:42:23 +0800

    init

---
+++ vscodeGit.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+typedef long long ll;
+using namespace std;
+static const auto io_sync_off = []() {
+    // turn off sync
+    std::ios::sync_with_stdio(false);
+    // untie in/out streams
+    std::cin.tie(nullptr);
+    return nullptr;
+}();
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+const int maxn = 100000 + 5;
+const int mod = 998244353;
+bool cmp(int a, int b) { return a > b; }
+
+int main()
+{
+    io_sync_off;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        cout << "This is a test of git from vscode!" << endl;
+    }
+    return 0;
+}
